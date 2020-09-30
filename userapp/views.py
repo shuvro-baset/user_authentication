e06ad46288ab123f7885b3d4ee4a2836c7862769
shuvro-baset <md.abdul.baset75@gmail.com>
2020-09-30 16:27:46 +0600

    reg

--- userapp/views.py
+++ userapp/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from userapp.forms import CreateUserForm
+from django.contrib import messages
 
 
-def home():
+def home(request):
     return render("this is home page", )
 
+
 def login(request):
-    return render(request, "login.html")
+    context = {}
+    return render(request, "login.html", context)
+
 
 def signup(request):
-    return render(request, "registration.html")
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account successfully create for ' + user)
+            return redirect('userapp:login')
+    context = {'form': form}
+    return render(request, "registration.html", context)

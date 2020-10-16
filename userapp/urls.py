e9af6ca5f9c256f1cd45c255504fcb261ae5f8a1
shuvro-baset <md.abdul.baset75@gmail.com>
2020-10-17 02:23:43 +0600

    logout data fetch done

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'userapp'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
     path('signup', views.signup, name='signup'),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,26 +1,57 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from userapp.forms import CreateUserForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
+@login_required(login_url='userapp:login')
 def home(request):
-    return render("this is home page", )
+    if request.user.is_authenticated:
+        all_users = get_user_model().objects.all()
+
+        context = {'allusers': all_users}
+    return render(request, 'home.html', context)
 
 
-def login(request):
-    context = {}
-    return render(request, "login.html", context)
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('userapp:home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('userapp:home')
+            else:
+                messages.info(request, 'Username or Password incorrect')
+                return render(request, "login.html", )
+        context = {}
+        return render(request, "login.html", context)
 
 
 def signup(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account successfully create for ' + user)
-            return redirect('userapp:login')
-    context = {'form': form}
-    return render(request, "registration.html", context)
+    if request.user.is_authenticated:
+        return redirect('userapp:home')
+    else:
+
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account successfully create for ' + user)
+                return redirect('userapp:login')
+        context = {'form': form}
+        return render(request, "registration.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('userapp:login')
